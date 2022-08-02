13a16a91dd2aef9140a2c2306c1bbde3be191287
Victor <Victor@DESKTOP-SKOFSMV>
2022-08-02 07:04:45 -0300

    Add project files.

---
+++ Exercicio10/ConversorDeMoeda.cs
@@ -0,0 +1,12 @@
+﻿namespace Exercicio10
+{
+    class ConversorDeMoeda
+    {
+        public static double Iof = 0.06;
+
+        public static double ValorReal(double quantidadeDolar, double cotacaoDolar)
+        {
+            return (quantidadeDolar + (quantidadeDolar * Iof))  * cotacaoDolar;
+        }
+    }
+}
